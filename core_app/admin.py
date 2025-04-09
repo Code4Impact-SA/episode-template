634107307149138a8415a464631da60ebcadde63
Kearabiloe Ledwaba <kearabiloe.ledwaba@gmail.com>
2025-04-09 08:19:05 +0200

    Add custom user model

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('bio', 'phone_number',)}),
+    )
--- core_app/models.py
+++ core_app/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    # Add your custom fields here
+    bio = models.TextField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
