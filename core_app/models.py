7f645e81b7bb940bbf0d01c051270c7c0e13288a
Kearabiloe Ledwaba <kearabiloe.ledwaba@gmail.com>
2025-04-09 08:26:18 +0200

    Minor edits

--- core_app/models.py
+++ core_app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.conf import settings
 
 class User(AbstractUser):
-    # Add your custom fields here
     bio = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
